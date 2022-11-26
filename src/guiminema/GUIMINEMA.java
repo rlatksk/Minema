6fff8c06d5c95f1f03592972fe4c18e0bbd082e6
rlatksk <102430406+rlatksk@users.noreply.github.com>
2022-11-26 23:06:35 +0700

    first

---
+++ src/guiminema/GUIMINEMA.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guiminema;
+
+/**
+ *
+ * @author Justin
+ */
+public class GUIMINEMA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home hm = new Home();
+        hm.setVisible(true);
+        hm.pack();
+        hm.setLocationRelativeTo(null);
+        hm.setDefaultCloseOperation(Home.EXIT_ON_CLOSE);
+    }
+    
+}
